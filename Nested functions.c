166ea77160efa2851036cbff6acb86fdb9de68b1
Aman Raj <160530800+rajamannn@users.noreply.github.com>
2024-11-03 00:47:09 +0530

    Add files via upload

---
+++ Nested functions.c
@@ -0,0 +1,20 @@
+// C program to illustrate the use of Nested function
+#include <stdio.h>
+
+int main(void)
+
+{
+  printf("Main");
+ int fun()
+ {
+   printf("fun");
+ int view()
+{
+   printf("view");
+}
+return 1;
+ }
+  view();
+return 0;
+}
+
